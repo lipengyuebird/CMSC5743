f5e04c76544eea49d641f68a20cde81b7924e301
perye <lipengyu.cn@hotmail.com>
2023-04-05 16:35:25 +0800

    add test script

--- main.cpp
+++ main.cpp
@@ -6,19 +6,22 @@
 #include "acc_function/winograd/WinogradFunction_1D.h"
 #include "feature_map/RegularSparseFeatureMap.h"
 #include <npy.hpp>
+#include <filesystem>
 
 
 int main() {
 
+    // =============== SET THE DATA PATH HERE ===============
+    std::string path = "/home/perye/CLionProjects/cmsc5743/Im2col/resources/pointcloud.npy";
+
     const int C = 64, W = 64, H = 64;
-    const int K = 256, R = 3, S = 3;
+    const int K = 64, R = 3, S = 3;
 
     // Set to true to view inner output
     const bool SHOW_OUTPUT = false;
 
     // Canonical feature map
-    DirectFeatureMap<double_t> * directFeatureMap  = DirectFeatureMap<double_t>::readNpy("/home/perye/CLionProjects/cmsc5724/Im2col/resources/pointcloud.npy");
-//    DirectFeatureMap<double_t> * directFeatureMap  = new DirectFeatureMap<double>(C, H, W);
+    DirectFeatureMap<double_t> * directFeatureMap = DirectFeatureMap<double_t>::readNpy(path);
     if (SHOW_OUTPUT) directFeatureMap->printArray();
 
     // Im2col feature map
@@ -57,8 +60,17 @@ int main() {
     }
 
     std::cout << "Direct conv: " << t1 - t0 << std::endl
-    << "Im2col conv: " << t2 - t1 << std::endl
-    << "Im2col conv with Winograd: " << t3 - t2 << std::endl
-    << "Sparse conv: " << t4 - t3 << std::endl;
+              << "Im2col conv: " << t2 - t1 << std::endl
+              << "Im2col conv with Winograd: " << t3 - t2 << std::endl
+              << "Sparse conv: " << t4 - t3 << std::endl;
+
+    // test
+    for (int k = 0; k < map1->K; ++k) {
+        for (int p = 0; p < map1->P; ++p) {
+            for (int q = 0; q < map1->Q; ++q) {
+                assert(map1->outputArray[k][p][q] == map4->outputArray[k][p][q]);
+            }
+        }
+    }
 
 }
